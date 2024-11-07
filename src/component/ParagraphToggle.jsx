e7c46742313cc62cf4a04faffaba1712d83386fa
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-07 12:52:36 +0530

    :zap: :lipstick: :sparkles: added styles

--- src/component/ParagraphToggle.jsx
+++ src/component/ParagraphToggle.jsx
@@ -3,7 +3,7 @@ import useToggle from "../hooks/useToggle";
 const ParagraphToggle = () => {
   const { toggle, handleToggle } = useToggle();
   return (
-    <div>
+    <div className="card">
       {toggle && <p id="my-paragraph">This is the paragraph you can toggle</p>}
 
       <button id="toggle-btn" onClick={handleToggle}>
